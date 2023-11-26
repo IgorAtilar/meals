951f2e876c74a3c68b23ec581203e98f4133ea1a
Igor Atilar <igoratilar10@gmail.com>
2023-11-26 19:34:23 -0300

    Add new files and classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:meals/utils/app_routes.dart';
+import './screens/categories_screen.dart';
+import './screens/categories_meals_screen.dart';
 
 void main() {
   runApp(const MainApp());
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Meals'),
-      ),
-      body: const Center(
-        child: Text('Hello world!'),
-      ),
-    );
-  }
 }
 
 class MainApp extends StatelessWidget {
@@ -33,9 +15,25 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       title: 'Meals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.pink,
+        ).copyWith(
+          secondary: Colors.amber,
+        ),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              titleLarge: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
       ),
-      home: const MyHomePage(),
+      initialRoute: '/',
+      routes: {
+        AppRoutes.home: (_) => const CategoriesScreen(),
+        AppRoutes.categoriesMeals: (_) => const CategoriesMealsScreen(),
+      },
     );
   }
 }
